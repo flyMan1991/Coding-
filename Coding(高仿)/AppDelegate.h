bda5b95864fb6faa1ef7cbd826e4c523d89c322c
cao.xin <cao.xin@cesgroup.com.cn>
2016-10-12 14:17:20 +0800

    第一次提交

---
+++ Coding(高仿)/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Coding(高仿)
+//
+//  Created by mac on 2016/10/12.
+//  Copyright © 2016年 CES. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Coding(高仿)/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Coding(高仿)
+//
+//  Created by mac on 2016/10/12.
+//  Copyright © 2016年 CES. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
